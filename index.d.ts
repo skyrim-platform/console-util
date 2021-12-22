5b2a4713808f8f43a07b9641e63e3660a99e9b5a
Mrowr Purr <mrowr.purr@gmail.com>
2021-12-21 18:46:18 -0800

    ConsoleUtil TypeScript library

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+import { ObjectReference } from "skyrimPlatform";
+export declare const SetSelectedReference: (a_reference: ObjectReference | null | undefined) => void;
+export declare const ExecuteCommand: (a_command: string) => void;
+export declare const PrintMessage: (a_message: string) => void;
+export declare const GetVersion: () => number;
+export declare const ReadMessage: () => string;
+export declare const GetSelectedReference: () => ObjectReference | null | undefined;
